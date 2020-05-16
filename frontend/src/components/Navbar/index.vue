2136717be18c516e1e4afa619f1e07610bd146bd
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-16 18:38:53 +0200

    add navbar

---
+++ frontend/src/components/Navbar/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-indigo-400 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">    
+      <span class="font-semibold text-xl tracking-tight font-mono">Youtube Playlist Sharing</span>
+    </div>
+  
+    <div class="flex justify-end">
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white lg:mt-0">Login</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "navbar"
+  };
+</script>
