d2d18f5db1f92a4cbf27fb523cfcc5da2f8ed35c
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-17 00:37:04 +0200

    add pseudo login

--- frontend/src/components/Navbar/index.vue
+++ frontend/src/components/Navbar/index.vue
@@ -1,17 +1,30 @@
 <template>
   <nav class="flex items-center justify-between flex-wrap bg-indigo-400 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">    
-      <span class="font-semibold text-xl tracking-tight font-mono">Youtube Playlist Sharing</span>
+      <router-link to="/" class="font-semibold text-xl tracking-tight font-mono">Youtube Playlist Sharing</router-link>
     </div>
   
     <div class="flex justify-end">
-      <a href="#" class="inline-block text-md px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white lg:mt-0">Login</a>
+      <button v-if="!this.$store.state.isLoggedIn" @click="handleLogin" class="inline-block text-md px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white lg:mt-0">Login</button>
+      <template v-if="this.$store.state.isLoggedIn">
+        <router-link to="/playlist/new" class="inline-block text-md mr-4 px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white lg:mt-0">Create new playlist</router-link>
+        <button @click="handleLogout" class="inline-block text-md px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white lg:mt-0">Logout</button>
+      </template>
     </div>
   </nav>
 </template>
 
 <script>
   export default {
-    name: "navbar"
+    name: "navbar",
+    methods: {
+      handleLogin: function () {
+        this.$store.commit('setIsLoggedIn', true);
+      },
+      handleLogout: function () { 
+        this.$store.commit('setIsLoggedIn', false);
+        this.$router.push({ path: '/' })
+      }
+    }
   };
 </script>
