51ea52dc171dc9efd9d663921c98d145b851d053
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-17 02:23:09 +0200

    add actual explanation text

--- frontend/src/components/Home/ExplanationCard.vue
+++ frontend/src/components/Home/ExplanationCard.vue
@@ -5,7 +5,12 @@
     <div class="w-full border-r border-b border-l-0 border-t border-gray-400 bg-white rounded-l-none rounded p-4 flex flex-col leading-normal">
       <div class="mb-8 h-48">
         <div class="text-left text-gray-900 font-bold text-base md:text-lg lg:text-xl mb-2">Create and share your youtube playlists with your friends</div>
-        <p class="text-gray-700 text-base">Lorem ipsum dolor sl.</p>
+        <ol class="list-decimal">
+          <li class="ml-5 text-left text-gray-700 text-base">Create an account or use your google account to login in</li>
+          <li class="ml-5 text-left text-gray-700 text-base">Create a playlist of your favorite videos</li>
+          <li class="ml-5 text-left text-gray-700 text-base">Share the link to your playlist with your friends</li>
+          <li class="ml-5 text-left text-gray-700 text-base">Your friends do not need an account to enjoy the playlist</li>
+        </ol>
       </div>
     </div>
   </div>
