03c4adffb7324b20cd4922e3cbd480955b4132c3
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-23 23:17:16 +0200

    remove playlistSample from home page due to it not being feasible with just dynamodb

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <explanation-card class="mb-10" />
-    <playlist-sample />
   </div>
 </template>
 
 <script>
   import ExplanationCard from '@/components/Home/ExplanationCard'
-  import PlaylistSample from '@/components/Home/PlaylistSample'
 
   export default {
     name: 'home',
     components: {
-      ExplanationCard,
-      PlaylistSample
+      ExplanationCard
     }
   }
 </script>
