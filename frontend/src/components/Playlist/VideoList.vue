75d15f21f009914e7bc97c227fc56910afb5247e
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-24 00:23:59 +0200

    finalize frontend by adding playlist deletion option

--- frontend/src/components/Playlist/VideoList.vue
+++ frontend/src/components/Playlist/VideoList.vue
@@ -27,6 +27,17 @@
         <input class="flex-grow bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-400" id="inline-playlist-name" type="file">
         <button class="ml-2 shadow bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Update</button>
       </div>
+
+      <div class="flex mt-10">
+        <button @click="toggleDeletionConfirmation" class="shadow bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Delete Playlist</button>
+        <div class="flex" v-if="showDeletionConfirmation">
+          <div class="ml-2 bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative" role="alert">
+            <strong class="font-bold">This action is permanent. Are you sure?</strong>
+          </div>
+          <button class="ml-2 shadow bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Yes</button>
+          <button @click="toggleDeletionConfirmation" class="ml-2 shadow bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Cancel</button>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
@@ -42,6 +53,16 @@
     },
     props: {
       playlist: PlaylistItem
+    },
+    data() {
+      return {
+        showDeletionConfirmation: false
+      }      
+    },
+    methods: {
+      toggleDeletionConfirmation() {
+        this.showDeletionConfirmation = !this.showDeletionConfirmation
+      }
     }
   }
 </script>
