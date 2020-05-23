5576be0767c2f335fa4abc90c3f944ccc63132bc
Michael Skrzypietz <michael.skr97@gmail.com>
2020-05-24 00:37:13 +0200

    fix error for not authenticated users

--- frontend/src/components/Playlist/VideoCard.vue
+++ frontend/src/components/Playlist/VideoCard.vue
@@ -6,7 +6,7 @@
       </a>
       <div class="flex px-4 py-4 items-center">
         <div class="flex-grow text-base">{{ video.caption }}</div>
-        <button v-if="this.$auth.user.sub === video.ownerId" class="flex ml-4 shadow bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded" type="button">Delete</button>
+        <button v-if="this.$auth.user && this.$auth.user.sub === video.ownerId" class="flex ml-4 shadow bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded" type="button">Delete</button>
       </div>
     </div>
   </div>
